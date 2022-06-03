65cc9d397f2722e5b12a1148ca1bdbe8351f0131
kennygomez <104158741+kennygomez@users.noreply.github.com>
2022-06-03 10:16:29 -0600

    main.js

---
+++ main.js
@@ -0,0 +1,198 @@
+const pokemonContainer = document.querySelector(".pokemon-container");
+const spinner = document.querySelector("#spinner");
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeId = document.querySelector('[data-poke-id]')
+
+const searchPokemon = event => {
+    event.preventDefault();
+    const { value } = event.target.pokemon;
+    const poke = event.target.pokemon;
+    console.log("estoy en la funcion");
+    console.log(poke.value);
+    console.log(`https://pokeapi.co/api/v2/pokemon/${poke.value}`)
+	  fetch(`https://pokeapi.co/api/v2/pokemon/${poke.value}`)
+        .then((res) => {
+          data.json();  
+          console.log(data);
+          createPokemon(data);
+        })
+        .then(response => {
+          console.log("estoy en el segundo then")
+          renderPokemonData(response);
+          createPokemon(res);
+          console.log(response.json());
+        })
+        .catch(err => renderNotFound())
+}
+
+
+const renderPokemonData = data => {
+    const sprite =  data.sprites.front_default;
+    const { stats, types } = data;
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent = `Nº ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
+}
+
+
+const setCardColor = types => {
+    const colorOne = typeColors[types[0].type.name];
+    const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+    pokeImg.style.background =  `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+    pokeImg.style.backgroundSize = ' 5px 5px';
+}
+
+const renderPokemonTypes = types => {
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        typeTextElement.style.color = typeColors[type.type.name];
+        typeTextElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+    });
+}
+
+const renderPokemonStats = stats => {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("div");
+        const statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        pokeStats.appendChild(statElement);
+    });
+}
+
+let limit = 8;
+let offset = 1;
+
+previous.addEventListener("click", () => {
+  if (offset != 1) {
+    offset -= 9;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+  }
+});
+
+next.addEventListener("click", () => {
+  offset += 9;
+  removeChildNodes(pokemonContainer);
+  fetchPokemons(offset, limit);
+});
+
+function fetchPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+      createPokemon(data);
+      spinner.style.display = "none";
+    });
+}
+
+function fetchPokemons(offset, limit) {
+  spinner.style.display = "block";
+  for (let i = offset; i <= offset + limit; i++) {
+    fetchPokemon(i);
+  }
+}
+
+function createPokemon(pokemon) {
+  const flipCard = document.createElement("div");
+  flipCard.classList.add("flip-card");
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card-container");
+
+  flipCard.appendChild(cardContainer);
+
+  const card = document.createElement("div");
+  card.classList.add("pokemon-block");
+
+  const spriteContainer = document.createElement("div");
+  spriteContainer.classList.add("img-container");
+
+  const sprite = document.createElement("img");
+  sprite.src = pokemon.sprites.front_default;
+
+  spriteContainer.appendChild(sprite);
+
+  const number = document.createElement("p");
+  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+  const name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = pokemon.name;
+
+  card.appendChild(spriteContainer);
+  card.appendChild(number);
+  card.appendChild(name);
+
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("pokemon-block-back");
+
+  cardBack.appendChild(progressBars(pokemon.stats));
+
+  cardContainer.appendChild(card);
+  cardContainer.appendChild(cardBack);
+  pokemonContainer.appendChild(flipCard);
+}
+
+function progressBars(stats) {
+  const statsContainer = document.createElement("div");
+  statsContainer.classList.add("stats-container");
+
+  for (let i = 0; i < 3; i++) {
+    const stat = stats[i];
+
+    const statPercent = stat.base_stat / 2 + "%";
+    const statContainer = document.createElement("stat-container");
+    statContainer.classList.add("stat-container");
+
+    const statName = document.createElement("p");
+    statName.textContent = stat.stat.name;
+
+    const progress = document.createElement("div");
+    progress.classList.add("progress");
+
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress-bar");
+    progressBar.setAttribute("aria-valuenow", stat.base_stat);
+    progressBar.setAttribute("aria-valuemin", 0);
+    progressBar.setAttribute("aria-valuemax", 200);
+    progressBar.style.width = statPercent;
+
+    progressBar.textContent = stat.base_stat;
+
+    progress.appendChild(progressBar);
+    statContainer.appendChild(statName);
+    statContainer.appendChild(progress);
+
+    statsContainer.appendChild(statContainer);
+  }
+
+  return statsContainer;
+}
+
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+fetchPokemons(offset, limit);
+
+const renderNotFound = () => {
+
+	
+
+}
